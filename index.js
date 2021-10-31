0a5a5148157f906ed27814312778ceb2a0c81f62
seph <seph@directionless.org>
2021-10-30 21:49:41 -0400

    fix includes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const fetch = require("node-fetch");
 
 const project_metadata_query = `
 query($org: String!, $number: Int!) {
